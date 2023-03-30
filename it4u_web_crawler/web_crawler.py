e76dce24b4da691b999d14c77d75636192a3884f
Louis Nguyen <thonh.it@gmail.com>
2023-03-30 20:41:55 +0700

    release new version

--- it4u_web_crawler/web_crawler.py
+++ it4u_web_crawler/web_crawler.py
@@ -1,13 +1,17 @@
-import threading
-import logging
-import requests
-import json
-import time
-import reactivex as rx
-from reactivex import Observable, operators as ops
-from typing import Dict, Any, Callable, List
-from it4u_http_request import HttpRequest, Url, default_cache_key_calculator
 import concurrent.futures
+import json
+import logging
+import threading
+import time
+from typing import Any, Callable, Dict, List, Tuple
+
+import reactivex as rx
+import requests
+from it4u_http_request import HttpRequest, Url, default_cache_key_calculator
+from reactivex import Observable
+from reactivex import operators as ops
+from tqdm import tqdm
+
 
 class WebCrawler:
     def __init__(self, max_threads: int = 16, session_builder: Callable[[], requests.Session] = lambda: requests.Session(), headers: Dict[str, str] = None):
@@ -18,42 +22,34 @@ class WebCrawler:
         self.max_threads: int = max_threads
         self.responses_lock = threading.Lock()
 
-    def crawl_data(self, responses: List[Any], semaphore, url: Url, pipes: List[Observable] = [], headers: Dict[str, str] = None, allow_redirects: bool = True, timeout: int = 5, max_retries: int = 3, proxy_builder: Callable[[], Dict[str, str]] = None) -> Any:
-        with semaphore:
-            try:
-                response = self.http_request \
-                    .get_response_stream(url=url,
-                                         pipes=pipes,
-                                         headers=headers,
-                                         allow_redirects=allow_redirects,
-                                         timeout=timeout,
-                                         max_retries=max_retries,
-                                         proxy_builder=proxy_builder)
+    def crawl_data(self, param: List[Tuple[Any, Url]], pipes: List[Observable] = [], headers: Dict[str, str] = None, allow_redirects: bool = True, timeout: int = 5, verify: bool = False, max_retries: int = 3, proxy_builder: Callable[[], Dict[str, str]] = None) -> Any:
+        urls: Url = [w[1] for w in param]
 
-                with self.responses_lock:
-                    responses.append(response)
+        return self.http_request \
+            .get_responses_stream(urls=urls,
+                                  pipes=pipes,
+                                  headers=headers,
+                                  allow_redirects=allow_redirects,
+                                  timeout=timeout,
+                                  verify=verify,
+                                  max_retries=max_retries,
+                                  proxy_builder=proxy_builder)
 
-                logging.info("Crawled data from: %s", url.url)
-            except requests.exceptions.RequestException as e:
-                logging.error("Error crawling data from: %s, %s", url.url, e)
-
-    def start_crawling(self, urls: List[Url], pipes: List[Observable] = [], headers: Dict[str, str] = None, allow_redirects: bool = True, timeout: int = 5, max_retries: int = 3, proxy_builder: Callable[[], Dict[str, str]] = None) -> List[Any]:
+    def start_crawling(self, list: List[List[Url]], pipes: List[Observable] = [], headers: Dict[str, str] = None, allow_redirects: bool = True, timeout: int = 5, max_retries: int = 3, verify: bool = False, proxy_builder: Callable[[], Dict[str, str]] = None) -> List[Any]:
         responses = []
-        semaphore = threading.Semaphore(self.max_threads)
         start_time = time.time()
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_threads) as executor:
             futures = []
-            for url in urls:
-                futures.append(executor.submit(self.crawl_data, responses, semaphore, url, pipes, headers, allow_redirects, timeout, max_retries, proxy_builder))
-
-            for future in concurrent.futures.as_completed(futures):
-                try:
+            for index, urls in enumerate(list):
+                futures.append(executor.submit(self.crawl_data, [(index * i, u) for i, u in enumerate(urls)], pipes, headers, allow_redirects, timeout, verify, max_retries, proxy_builder))
+            
+            with tqdm(total=len(list), desc=f"Fetching") as pbar:
+                for future in concurrent.futures.as_completed(futures):
                     response = future.result()
                     if response is not None:
                         responses.append(response)
-                except Exception as e:
-                    logging.error(f"An exception occurred while crawling {url}: {e}")
+                    pbar.update(1)
 
         end_time = time.time()
         logging.info("All threads have finished.")
--- test.py
+++ test.py
@@ -4,7 +4,9 @@ import random
 from it4u_web_crawler import WebCrawler
 from it4u_http_request import Url, proxies
 from reactivex import operators as ops
+import urllib3
 
+urllib3.disable_warnings()
 logging.basicConfig(level=logging.INFO, filename='out.log')
 
 if __name__ == '__main__':
@@ -19,16 +21,16 @@ if __name__ == '__main__':
             'https://masothue.com/0110256646-cong-ty-tnhh-suneva-viet-nam',
             'https://masothue.com/0110256639-cong-ty-tnhh-thuong-mai-dich-vu-an-phong-khanh',
             'https://masothue.com/0317691518-cong-ty-tnhh-mimisa-beauty-226-clinic']
-    urls = [Url(u) for u in urls]
+    urls = [[Url(u)] for u in urls]
 
     crawler = WebCrawler(max_threads=8, session_builder=lambda: requests.Session())  # Limit to 5 parallel requests
     responses, execution_time = crawler.start_crawling(urls, [
         ops.filter(lambda res: res is not None),
-        ], proxy_builder=lambda: random.choice(proxies))
+        ], proxy_builder=lambda: random.choice(proxies), verify=False)
     
-    print (len(responses))
+    print (len(responses), [r[0][0].url for r in responses])
     # for response in responses:
     #     print("Response JSON:", response)
 
     print("Execution time:", execution_time)
-    print(responses[0])
+    # print(responses[0])
